55e5eb1e62b23f1bf0ee402538261ea9878f0740
sabrinaelkins <sabrina.elkins12@gmail.com>
2021-12-23 14:56:59 -0600

    sign up sheet and login sheet

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,28 +3,22 @@ const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+  
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      projects, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    res.render('homepage')
+});
+router.get('/signup', async (req, res) => {
+  
+
+  // Pass serialized data and session flag into template
+  res.render('signup')
+});
+router.get('/login', async (req, res) => {
+  
+
+  // Pass serialized data and session flag into template
+  res.render('login')
 });
 
 router.get('/project/:id', async (req, res) => {
