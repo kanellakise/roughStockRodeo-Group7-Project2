4f167ec58c60c4817fc7692d508f47c1e2c3a706
DeathByDusk <elijahkanellakis@gmail.com>
2022-01-07 20:33:55 -0600

    separated login and signup pages

---
+++ public/javascript/signup.js
@@ -0,0 +1,24 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#userInfo').value.trim();
+    const email = document.querySelector('#emailGetter').value.trim();
+    const password = document.querySelector('#passwordGetter').value.trim();
+    alert(username)
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
